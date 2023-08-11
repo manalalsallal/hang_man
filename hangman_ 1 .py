daaed99c96ff4233c7a8f9b4e86a4d726c43595c
manalalsallal <140978520+manalalsallal@users.noreply.github.com>
2023-08-11 22:13:43 +0300

    Create hangman_ 1 .py

---
+++ hangman_ 1 .py
@@ -0,0 +1,45 @@
+import random
+
+import time
+
+name = input("What is your name? ")
+
+print ("Hello, " + name, "Time to play hangman!")
+
+time.sleep(1)
+
+def hangman():
+    words = ["python", "java script", "html", "css"]
+    word = random.choice(words)
+    guessed_letters = []
+    returns = 10
+
+    while returns > 0:
+        guessed_word = ""
+        for letter in word:
+            if letter in guessed_letters:
+                guessed_word += letter
+            else:
+                guessed_word += "_"
+
+        if guessed_word == word:
+            print(" You are Win ")
+            break
+
+        print(guessed_word)
+
+        guess = input("\n please Enter letter :  ")
+       
+        if guess in word:
+            guessed_letters.append(guess)
+            print("true")
+        else:
+            returns -= 1
+            print(f" Wrong still {returns} returns.")
+
+    if returns == 0:
+        print(f"You loss")
+
+
+hangman()
+
